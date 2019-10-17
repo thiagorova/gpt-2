958052faaaaa9fc79988804d61766397509d85a6
Thiago Rodrigues Vasconcellos <thiagorova@gmail.com>
2019-10-17 16:53:49 -0300

    logging + starting wait after proper setup

--- src/server/app.py
+++ src/server/app.py
@@ -10,12 +10,15 @@ from commons.text_model_singleton import TextGeneratorSingleton as tgs
 
 if __name__ == "__main__":
     enable_pretty_logging()
+    print('Starting system setup')
     ARGS = sys.argv
     ARGS.append("--log_file_prefix=my_app.log")
     tornado.options.parse_command_line(ARGS)
 
     APP = tornado.web.Application(handlers=ROUTES)
     APP.listen(8892, max_buffer_size=1000000000)
-    tornado.ioloop.IOLoop.current().start()
+    print('Starting model training')
     tgs.init_model()
+    print('System ready to roll')
+    tornado.ioloop.IOLoop.current().start()
 
